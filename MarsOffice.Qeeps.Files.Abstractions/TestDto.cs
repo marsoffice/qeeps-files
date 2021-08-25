4ce71b127f2b90c66552afd9f4e3b1abfb3cbe56
Alin Florin <alin@qoffice.ro>
2021-08-25 16:33:59 +0300

    r

---
+++ MarsOffice.Qeeps.Files.Abstractions/TestDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarsOffice.Qeeps.Files.Abstractions
+{
+    public class TestDto
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ MarsOffice.Qeeps.Files/Startup.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+[assembly: FunctionsStartup(typeof(MarsOffice.Qeeps.Files.Startup))]
+namespace MarsOffice.Qeeps.Files
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            FunctionsHostBuilderContext context = builder.GetContext();
+            var env = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Development";
+            builder.ConfigurationBuilder
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{env}.json"), optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables();
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
